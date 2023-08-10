ce98fa67a255548600fed20e5f998d427d5b0165
Lauren Owens <lo2175@stern.nyu.edu>
2023-08-10 14:21:29 -0400

    Update 2

--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -7,11 +7,11 @@ home_routes = Blueprint("home_routes", __name__)
 @home_routes.route("/")
 @home_routes.route("/home")
 def index():
-    print("HOME...")
+    print("Farmer's Market Finder...")
     #return "Welcome Home"
     return render_template("home.html")
 
-@home_routes.route("/finder")
+@home_routes.route("/seasonality")
 def about():
-    print("Farmer's Market Finder...")
-    return render_template("finder.html")
+    print("Seasonality...")
+    return render_template("seasonality.html")
